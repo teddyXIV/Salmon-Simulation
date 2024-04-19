fa296a842f293fccae61d894c7a268cf2a8f1389
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-19 11:35:09 -0700

    format map

--- redd/src/components/RiverMap.tsx
+++ redd/src/components/RiverMap.tsx
@@ -3,7 +3,7 @@ import Map from "@arcgis/core/Map";
 import { useEffect, useRef } from "react";
 
 const RiverMap = () => {
-    const mapRef = useRef<HTMLDivElement>(null)
+    const mapRef = useRef(null)
 
     useEffect(() => {
         if (!mapRef?.current) return;
@@ -14,8 +14,9 @@ const RiverMap = () => {
         const view = new MapView({
             map: map,
             container: mapRef.current,
-            center: [-121.9434, 45.6411],
+            center: [-121.35, 46.25],
             zoom: 8.5,
+            // scale: 54000
         });
         return () => {
             view.destroy()
@@ -23,7 +24,9 @@ const RiverMap = () => {
     }, [])
 
     return (
-        <div className="h-96 w-80 bg-orange-400" ref={mapRef}></div>
+        <div className="flex justify-center items-center h-screen">
+            <div className="h-1/2 w-5/6 bg-green-900" ref={mapRef}></div>
+        </div>
     )
 }
 
