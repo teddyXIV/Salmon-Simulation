1aaed2543e8429fcf28e2b7982cc7b2cf238953e
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-19 16:34:54 -0700

    wip rendering dam locations on map

--- redd/src/components/RiverMap.tsx
+++ redd/src/components/RiverMap.tsx
@@ -1,5 +1,9 @@
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
 import { useEffect, useRef } from "react";
 
 const RiverMap = () => {
@@ -18,14 +22,34 @@ const RiverMap = () => {
             zoom: 8.5,
             // scale: 54000
         });
+
+        const graphicsLayer = new GraphicsLayer();
+        view.map.add(graphicsLayer);
+
+        const point = new Point({
+            longitude: -121.941,
+            latitude: 45.645,
+        });
+
+        const marker = new SimpleMarkerSymbol({
+            color: "green",
+        })
+
+        const damPoint = new Graphic({
+            geometry: point,
+            symbol: marker
+        });
+
+        graphicsLayer.add(damPoint);
+
         return () => {
             view.destroy()
         }
     }, [])
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="h-1/2 w-5/6 bg-green-900" ref={mapRef}></div>
+        <div className="flex justify-center items-center h-screen bg-green-600">
+            <div className="h-[500px] w-full lg:w-5/6 bg-green-900" ref={mapRef}></div>
         </div>
     )
 }
