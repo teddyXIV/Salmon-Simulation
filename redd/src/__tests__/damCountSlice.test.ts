cd1502904302221bc3802b8ee19eea00bbd36458
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-22 10:43:16 -0700

    add wip test

--- redd/src/__tests__/damCountSlice.test.ts
+++ redd/src/__tests__/damCountSlice.test.ts
@@ -1,9 +1,57 @@
-import damCountReducer, { setCount, clearData } from '../redux/damCountSlice'
+import damCountReducer, { setCount } from '../redux/damCountSlice'
 import { expect, describe, test } from 'vitest'
 
 describe("damCountSlice", () => {
 
-    const dataObject = {
+    const testData = [
+        {
+            date: "2023-07-01",
+            count: 1,
+            dam_id: 1
+        },
+        {
+            date: "2023-07-01",
+            count: 2,
+            dam_id: 2
+        },
+        {
+            date: "2023-07-01",
+            count: 3,
+            dam_id: 3
+        },
+        {
+            date: "2023-07-01",
+            count: 4,
+            dam_id: 9
+        }
+    ];
 
-    }
+    test("Should add the date and count values to the correct dam state slice", () => {
+        const initialState = {
+            bon: [],
+            tda: [],
+            jda: [],
+            mcn: [],
+            prd: [],
+            wan: [],
+            ris: [],
+            rrh: [],
+            wel: []
+        }
+
+        const expected = {
+            bon: [{ date: "2023-07-01", salmon_count: 1 }],
+            tda: [{ date: "2023-07-01", salmon_count: 2 }],
+            jda: [{ date: "2023-07-01", salmon_count: 3 }],
+            mcn: [],
+            prd: [],
+            wan: [],
+            ris: [],
+            rrh: [],
+            wel: [{ date: "2023-07-01", salmon_count: 4 }]
+        }
+
+        const updatedState = damCountReducer(initialState, setCount(testData))
+        expect(updatedState).toEqual({ expected })
+    })
 })
--- redd/src/redux/damCountSlice.ts
+++ redd/src/redux/damCountSlice.ts
@@ -15,13 +15,14 @@ interface DamCounts {
 }
 
 interface SalmonCount {
-    date: Date,
+    date: string,
     salmon_count: number
 }
 
 interface DamUpdate {
-    dam: number,
-    data: SalmonCount
+    date: string,
+    count: number,
+    dam_id: number
 }
 
 const initialState: DamCounts = {
@@ -40,38 +41,29 @@ export const damCountSlice = createSlice({
     name: 'damCount',
     initialState,
     reducers: {
-        setCount: (state: DamCounts, action: PayloadAction<DamUpdate>) => {
-            const countData = action.payload
-            switch (action.payload.dam) {
-                case 1:
-                    state.bon.push(countData.data);
-                    break;
-                case 2:
-                    state.tda.push(countData.data);
-                    break;
-                case 3:
-                    state.jda.push(countData.data);
-                    break;
-                case 4:
-                    state.mcn.push(countData.data)
-                    break;
-                case 5:
-                    state.prd.push(countData.data)
-                    break;
-                case 6:
-                    state.wan.push(countData.data)
-                    break;
-                case 7:
-                    state.ris.push(countData.data)
-                    break;
-                case 8:
-                    state.rrh.push(countData.data)
-                    break;
-                default:
-                    state.wel.push(countData.data)
-                    break;
-            }
+        setCount: (state: DamCounts, action: PayloadAction<DamUpdate[]>) => {
 
+            action.payload.forEach(el => {
+                const storeData = {
+                    date: el.date,
+                    salmon_count: el.count,
+                }
+
+                const damMap: Record<number, keyof DamCounts> = {
+                    1: 'bon',
+                    2: 'tda',
+                    3: 'jda',
+                    4: 'mcn',
+                    5: 'prd',
+                    6: 'wan',
+                    7: 'ris',
+                    8: 'rrh',
+                    9: 'wel'
+                }
+
+                const damKey = damMap[el.dam_id];
+                state[damKey].push(storeData)
+            });
         },
         clearData: (state: DamCounts) => {
             state.bon = [];
