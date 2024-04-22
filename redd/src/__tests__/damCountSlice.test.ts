01073955a4ac6f2cb26c5531523db490901ec6dc
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-22 09:33:51 -0700

    add wip test for reducers

---
+++ redd/src/__tests__/damCountSlice.test.ts
@@ -0,0 +1,9 @@
+import damCountReducer, { setCount, clearData } from '../redux/damCountSlice'
+import { expect, describe, test } from 'vitest'
+
+describe("damCountSlice", () => {
+
+    const dataObject = {
+
+    }
+})
