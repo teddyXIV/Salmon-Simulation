bb2fa141d1610f5096cef5907facf7ba1668d9cc
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-30 10:09:42 -0700

    add styling

--- redd/src/components/Header.tsx
+++ redd/src/components/Header.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
     return (
-        <div className="font-sans text-white text-5xl bg-black/60 w-screen h-44 absolute inset-x-0 top-0">
-            <h1 className="text-center my-10">Salmon Sim</h1>
+        <div className=" flex flex-row font-sans text-white bg-black/60 w-screen h-fit absolute inset-x-0 top-0">
+            <h1 className="text-3xl p-3 w-2/3">Columbia River Salmon Passage</h1>
+            <div className="flex flex-row w-1/3 text-center">
+                <h3 className="text-right my-auto w-1/2">About this project</h3>
+                <h3 className="text-right my-auto w-1/2 mr-10">Data</h3>
+            </div>
         </div>
     )
 }
